a341f81f50ed6a9e912ee899984caf89a95e1a83
ewrefads <clara.donovan@outlook.com>
2024-11-11 08:18:44 +0100

    Added DDA class

---
+++ Assets/Scripts/CORE/DDA/DynamicDifficultyAdjustment.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicDifficultyAdjustment : MonoBehaviour
+{
+    List<string> words;
+    List<string> letters;
+    List<string> properties;
+    int playerLanguageLevel;
+
+    public void GetLetter(List<string> properties) {
+
+    }
+
+    public void GetLetters(List<string> properties) {
+        
+    }
+
+    public void GetWord(List<string> properties) {
+
+    }
+
+    public void GetWords(List<string> properties) {
+        
+    }
+
+    public void AdjustWeight(string languageUnit)
+    {
+        
+    }
+
+    public bool IsLanguageUnitTypeUnlocked(string property)
+    {
+        return true;
+    }
+
+    public List<string> GetPlayerPriorityUnlocked()
+    {
+        return new List<string>();
+    }
+
+    private void Load()
+    {
+
+    } 
+
+    private void Save()
+    {
+
+    }
+
+    private void SetupLanguageUnits()
+    {
+
+    }
+
+    private void CalculateLanguageLevel()
+    {
+
+    }
+}
