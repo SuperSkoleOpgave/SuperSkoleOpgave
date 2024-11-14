c3b88e948e69da88694b3303056fa3d050c0f680
Henr9423 <54700@student.eadania.dk>
2024-11-14 10:14:23 +0100

    Try Catch added

--- Assets/Scenes/50-Minigames/511-PizzaRestaurant/Scripts/PizzaRestaurantManager.cs
+++ Assets/Scenes/50-Minigames/511-PizzaRestaurant/Scripts/PizzaRestaurantManager.cs
@@ -75,91 +75,97 @@ public class PizzaRestaurantManager : MonoBehaviour
     void StartNewRound()
     {
 
-
-        TextMeshProUGUI text= textIngredientHolder.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
-
-        wordToGuess = WordsForImagesManager.GetRandomWordForImage();
-        ///ÚSE LATER WHEN IMAGES CAN BE LOADED SO WHEN THE GAME IS IN THE MAINWORLD !!!!!!
-        ImageDisplay.texture = ImageManager.GetImageFromWord(wordToGuess);
-        
-        
-     
-        currentLetterToGuessIndex = 0;
-        ingredientChecker.currentLetterToGuess = wordToGuess[currentLetterToGuessIndex];
-        
-
-        int numberOfRandomPositions = wordToGuess.Length;
-
-        int assignedCorrectLetters = 0;
-
-
-        for (int i = 0; i < 12; i++)
+        try
         {
-            int rndIndexX = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(0));
-            int rndIndexY = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(1));
-           
+            TextMeshProUGUI text = textIngredientHolder.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        
+
+            wordToGuess = WordsForImagesManager.GetRandomWordForImage();
+            ///ÚSE LATER WHEN IMAGES CAN BE LOADED SO WHEN THE GAME IS IN THE MAINWORLD !!!!!!
+            ImageDisplay.texture = ImageManager.GetImageFromWord(wordToGuess);
 
 
-           //Code to assign random positions for the letters of the correct word on the ingredientboard. 
-            if (assignedCorrectLetters<numberOfRandomPositions)
+
+            currentLetterToGuessIndex = 0;
+            ingredientChecker.currentLetterToGuess = wordToGuess[currentLetterToGuessIndex];
+
+
+            int numberOfRandomPositions = wordToGuess.Length;
+
+            int assignedCorrectLetters = 0;
+
+
+            for (int i = 0; i < 12; i++)
             {
-                while (lettersForCurrentRound[rndIndexX, rndIndexY] != '\0')
+                int rndIndexX = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(0));
+                int rndIndexY = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(1));
+
+
+
+                //Code to assign random positions for the letters of the correct word on the ingredientboard. 
+                if (assignedCorrectLetters < numberOfRandomPositions)
                 {
-                    rndIndexX = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(0));
-                    rndIndexY = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(1));
+                    while (lettersForCurrentRound[rndIndexX, rndIndexY] != '\0')
+                    {
+                        rndIndexX = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(0));
+                        rndIndexY = UnityEngine.Random.Range(0, lettersForCurrentRound.GetLength(1));
+
+                    }
+                    lettersForCurrentRound[rndIndexX, rndIndexY] = wordToGuess[assignedCorrectLetters];
+                    assignedCorrectLetters++;
 
                 }
-                lettersForCurrentRound[rndIndexX,rndIndexY] = wordToGuess[assignedCorrectLetters];
-                assignedCorrectLetters++;
-              
+                else
+                {
+                    //Code to assign random letters places on the ingredientboard. 
+                    for (int x = 0; x < lettersForCurrentRound.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < lettersForCurrentRound.GetLength(1); y++)
+                        {
+
+                            if (lettersForCurrentRound[x, y] == '\0')
+                            {
+                                lettersForCurrentRound[x, y] = LetterManager.GetRandomLetter();
+
+                                break;
+                            }
+                        }
+
+                    }
+                }
             }
-            else
+
+
+
+
+            // Code to actually instantiate the letters for the current round that have been put together in a random sequence. 
+            for (int y = 0; y < lettersForCurrentRound.GetLength(1); y++)
             {
-                //Code to assign random letters places on the ingredientboard. 
                 for (int x = 0; x < lettersForCurrentRound.GetLength(0); x++)
                 {
-                    for (int y = 0; y < lettersForCurrentRound.GetLength(1); y++)
-                    {
-                           
-                        if (lettersForCurrentRound[x,y]== '\0')
-                        {
-                            lettersForCurrentRound[x,y] = LetterManager.GetRandomLetter();
-                           
-                            break;
-                        }
-                    }
-                   
+                    text.text = lettersForCurrentRound[x, y].ToString();
+
+
+                    Vector3 pos = new Vector3(12 * x, y * -12, 0);
+
+
+
+                    GameObject instObject = Instantiate(textIngredientHolder, ingredientBoard.transform);
+
+                    instObject.transform.position += pos;
+                    instObject.GetComponent<IngredientHolderPickup>().startPosition = instObject.transform.position;
+                    instObject.GetComponent<IngredientHolderPickup>().ingredientChecker = ingredientChecker;
+                    spawnedLetters.Add(instObject);
                 }
+
             }
         }
 
-
-        
-
-        // Code to actually instantiate the letters for the current round that have been put together in a random sequence. 
-        for (int y = 0; y < lettersForCurrentRound.GetLength(1); y++)
+        catch (Exception ex)
         {
-            for (int x = 0; x < lettersForCurrentRound.GetLength(0); x++)
-            {
-                text.text = lettersForCurrentRound[x,y].ToString();
-
-
-                Vector3 pos = new Vector3(12 *x, y*-12, 0);
-
-
-               
-                GameObject instObject = Instantiate(textIngredientHolder, ingredientBoard.transform);
-
-                instObject.transform.position += pos;
-                instObject.GetComponent<IngredientHolderPickup>().startPosition = instObject.transform.position;
-                instObject.GetComponent<IngredientHolderPickup>().ingredientChecker = ingredientChecker;
-                spawnedLetters.Add(instObject);
-            }
+            Debug.LogError($"Failed to initialize round: {ex.Message}");
             
         }
-
-
-        
-
     }
 }
