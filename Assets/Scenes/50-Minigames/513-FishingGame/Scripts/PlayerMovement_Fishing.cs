3c67bab22a78c13018d62851a1832b6b29abf5a6
Henr9423 <54700@student.eadania.dk>
2024-11-25 15:47:33 +0100

    Movement and cast fishing pole

---
+++ Assets/Scenes/50-Minigames/513-FishingGame/Scripts/PlayerMovement_Fishing.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_Fishing : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Rigidbody2D rigidbody;
+
+    [SerializeField] int playerAccelleration=50;
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetAxis("Horizontal")>0f)
+        {
+            rigidbody.AddForce(new Vector2(transform.right.x, transform.right.y) * playerAccelleration);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            rigidbody.AddForce(-new Vector2(transform.right.x, transform.right.y) * playerAccelleration);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+        
+    }
+}
---
+++ Assets/Scenes/50-Minigames/513-FishingGame/Scripts/UseFishingRod.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseFishingRod : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject fishingLine;
+
+    private bool lineIsRolledOut;
+    private bool useFishingRod;
+
+    private float yScale;
+
+    [SerializeField] float maxYScale = 7.2f;
+    [SerializeField] float minYScale = 0.8f;
+
+   private Vector3 lineScale;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            useFishingRod = true;
+        }
+
+        if (lineIsRolledOut == false && useFishingRod==true)
+        {
+            yScale += 0.01f;
+            fishingLine.transform.localScale=new Vector3(1f,minYScale+yScale,1f);
+
+            if(yScale>=maxYScale)
+            {
+                lineIsRolledOut = true;
+
+                useFishingRod = false;
+                yScale = 0;
+            }
+        }
+
+        if(lineIsRolledOut==true && useFishingRod==true)
+        {
+            yScale -=0.01f;
+
+            lineScale = new Vector3(1f, maxYScale + yScale, 1f);
+
+            fishingLine.transform.localScale = lineScale;
+
+            if(lineScale.y<=minYScale)
+            {
+                lineIsRolledOut = false;
+                useFishingRod = false;
+                yScale = 0;
+            }
+
+        }
+
+    }
+}
