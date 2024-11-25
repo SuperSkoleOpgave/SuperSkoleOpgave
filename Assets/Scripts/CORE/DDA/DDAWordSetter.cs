7a9454940b8607ceecb17cbfa551d945cc79a4b1
Wingmoon5150 <54482@student.eadania.dk>
2024-11-25 10:50:19 +0100

    Added comment

--- Assets/Scripts/CORE/DDA/DDAWordSetter.cs
+++ Assets/Scripts/CORE/DDA/DDAWordSetter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DDAWordSetter : MonoBehaviour
 {
     [SerializeField]
@@ -14,18 +15,22 @@ public class DDAWordSetter : MonoBehaviour
     List<Texture2D> softDWords;
     [SerializeField]
     List<Texture2D> silentConsonantWords;
+
     void Start()
     {
         List<LanguageUnit> words = applyLanguageUnits();
     }
-
+    /// <summary>
+    /// goes through all words added to the words list and turns them into LanguageUnits
+    /// </summary>
+    /// <returns></returns>
     private List<LanguageUnit> applyLanguageUnits()
     {
         List<LanguageUnit> returnedUnits = new List<LanguageUnit>();
         foreach(Texture2D word in words)
         {
             LanguageUnit wordUnit = new LanguageUnit();
-            wordUnit.identifier = word.name;
+            wordUnit.identifier = word.name.ToLower();
             List<LanguageUnitProperty> props = new List<LanguageUnitProperty>();
             if (vowelConfusedWords.Contains(word)) props.Add(LanguageUnitProperty.vowelConfuse);
             if (doubleConsonantWords.Contains(word)) props.Add(LanguageUnitProperty.doubleConsonant);
